bd5bcede4b5b1bb62298bb30b9d10e569800785f
Epicadk <adityakurkure@gmail.com>
2020-09-06 21:22:47 +0530

    Added bottom Navigation

--- app/src/main/java/com/tip/lunchbox/view/activity/MainActivity.java
+++ app/src/main/java/com/tip/lunchbox/view/activity/MainActivity.java
@@ -1,16 +1,29 @@
 package com.tip.lunchbox.view.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
 import com.tip.lunchbox.R;
+import com.tip.lunchbox.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding mainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        //Binding Initialized
+        mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mainBinding.getRoot());
+        //Setting navController
+        NavController navController = Navigation.findNavController(this, R.id.navigation_host_fragment);
+        NavigationUI.setupWithNavController(mainBinding.mainActivityBn,navController);
+        NavigationUI.setupActionBarWithNavController(this,navController);
     }
 }
---
+++ app/src/main/java/com/tip/lunchbox/view/fragment/SearchFragment.java
@@ -0,0 +1,28 @@
+package com.tip.lunchbox.view.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.tip.lunchbox.R;
+
+public class SearchFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+}
