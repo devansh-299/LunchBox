20876ee6d5dbb9c1533228d7055ce2ccbd839e4e
devansh-299 <f20180131@goa.bits-pilani.ac.in>
2020-09-16 22:06:24 +0530

    integrated map sdk and added bottomsheet inside home fragment

--- app/src/main/java/com/tip/lunchbox/view/fragment/HomeFragment.java
+++ app/src/main/java/com/tip/lunchbox/view/fragment/HomeFragment.java
@@ -5,21 +5,21 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.tip.lunchbox.view.adapter.RestaurantAdapter;
 import com.tip.lunchbox.databinding.FragmentHomeBinding;
 import com.tip.lunchbox.viewmodel.HomeViewModel;
 
-public class HomeFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
+public class HomeFragment extends Fragment {
 
     HomeViewModel viewModel;
     FragmentHomeBinding homeBinding;
     RestaurantAdapter adapter;
+    private BottomSheetBehavior mBottomSheetBehavior;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -32,14 +32,13 @@ public class HomeFragment extends Fragment implements SwipeRefreshLayout.OnRefre
                              Bundle savedInstanceState) {
         homeBinding = FragmentHomeBinding.inflate(inflater, container, false);
 
-        homeBinding.swipeRefresh.setOnRefreshListener(this);
         View view = homeBinding.getRoot();
         viewModel = new ViewModelProvider(this).get(HomeViewModel.class);
         adapter = new RestaurantAdapter(getActivity());
-
+        mBottomSheetBehavior = BottomSheetBehavior.from(homeBinding.nsvRestaurantList);
         homeBinding.rvRestaurant.setLayoutManager(new LinearLayoutManager(getActivity()));
         homeBinding.rvRestaurant.setAdapter(adapter);
-        onRefresh();
+        loadData();
         return view;
     }
 
@@ -50,21 +49,19 @@ public class HomeFragment extends Fragment implements SwipeRefreshLayout.OnRefre
     }
 
     private void showErrorView() {
-        homeBinding.swipeRefresh.setRefreshing(false);
         homeBinding.rvRestaurant.setVisibility(View.GONE);
         homeBinding.pbLoading.setVisibility(View.GONE);
         homeBinding.llErrorOccurred.setVisibility(View.VISIBLE);
     }
 
     private void showData() {
-        homeBinding.swipeRefresh.setRefreshing(false);
         homeBinding.pbLoading.setVisibility(View.GONE);
         homeBinding.llErrorOccurred.setVisibility(View.GONE);
         homeBinding.rvRestaurant.setVisibility(View.VISIBLE);
     }
 
-    @Override
-    public void onRefresh() {
+
+    private void loadData() {
         showLoadingView();
         viewModel.getRestaurantLiveData().observe(getViewLifecycleOwner(), searchResponse -> {
             if(searchResponse != null) {
