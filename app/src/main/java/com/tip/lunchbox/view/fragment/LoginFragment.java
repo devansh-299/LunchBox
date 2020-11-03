591f6dc3f8db2f2497996313168d604c0a7cdf25
Epicadk <adityakurkure@gmail.com>
2020-11-03 20:18:22 +0530

    Feature: Added logic to Login Fragment

--- app/src/main/java/com/tip/lunchbox/view/fragment/LoginFragment.java
+++ app/src/main/java/com/tip/lunchbox/view/fragment/LoginFragment.java
@@ -1,6 +1,7 @@
 package com.tip.lunchbox.view.fragment;
 
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,8 +11,9 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.tip.lunchbox.databinding.FragmentLoginBinding;
+import com.tip.lunchbox.utilities.Validator;
 
-public class LoginFragment extends Fragment {
+public class LoginFragment extends Fragment implements View.OnClickListener {
     FragmentLoginBinding binding;
 
     @Nullable
@@ -21,5 +23,33 @@ public class LoginFragment extends Fragment {
                              @Nullable Bundle savedInstanceState) {
         binding = FragmentLoginBinding.inflate(inflater, container, false);
         return binding.getRoot();
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == binding.btLogin) {
+            setEnabled(false);
+            Boolean unValid = Validator.usernameValidator(binding
+                    .etUsername.getText().toString());
+            Boolean passValid = Validator.passwordValidator(binding
+                    .etPassword.getText().toString());
+            if (!unValid) {
+                binding.tiUsername.setError("Invalid Username");
+                setEnabled(true);
+            }
+            if (!passValid) {
+                binding.tiUsername.setError("Invalid Password");
+                setEnabled(true);
+            }
+            if (unValid && passValid) {
+                //Send Req to backend here
+            }
+        }
+    }
+
+    private void setEnabled(Boolean bool) {
+        binding.etPassword.setEnabled(bool);
+        binding.etUsername.setEnabled(bool);
+        binding.btLogin.setEnabled(bool);
     }
 }
