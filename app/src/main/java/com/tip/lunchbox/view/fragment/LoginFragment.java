0af4401b2442217c00195b58bd56816ef58216f1
Epicadk <adityakurkure@gmail.com>
2020-11-03 19:50:01 +0530

    Feature: Created Login Fragment

---
+++ app/src/main/java/com/tip/lunchbox/view/fragment/LoginFragment.java
@@ -0,0 +1,25 @@
+package com.tip.lunchbox.view.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.tip.lunchbox.databinding.FragmentLoginBinding;
+
+public class LoginFragment extends Fragment {
+    FragmentLoginBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        binding = FragmentLoginBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+}
