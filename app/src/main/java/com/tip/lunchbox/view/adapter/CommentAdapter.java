e2d85d2d779fe00f329716074856e9a6b3528593
Epicadk <adityakurkure@gmail.com>
2020-11-16 01:19:28 +0530

    Feature : Added comments adapter

---
+++ app/src/main/java/com/tip/lunchbox/view/adapter/CommentAdapter.java
@@ -0,0 +1,57 @@
+package com.tip.lunchbox.view.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.tip.lunchbox.databinding.ItemCommentsBinding;
+import com.tip.lunchbox.model.server.response.CommentsResponse;
+
+import java.util.List;
+
+public class CommentAdapter extends RecyclerView.Adapter<CommentAdapter.CommentsViewHolder> {
+    Context context;
+    List<CommentsResponse> commentsResponses;
+
+    public CommentAdapter(Context context) {
+        this.context = context;
+    }
+
+    public void setData(List<CommentsResponse> commentsResponses){
+        this.commentsResponses = commentsResponses;
+    }
+    @NonNull
+    @Override
+    public CommentAdapter.CommentsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new CommentsViewHolder(
+                ItemCommentsBinding.inflate(LayoutInflater.from(context), parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CommentAdapter.CommentsViewHolder holder, int position) {
+        holder.setData(commentsResponses.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return commentsResponses == null ? 0 : commentsResponses.size();
+    }
+
+    public class CommentsViewHolder extends RecyclerView.ViewHolder {
+        private ItemCommentsBinding commentsBinding;
+
+        public CommentsViewHolder(@NonNull ItemCommentsBinding binding) {
+            super(binding.getRoot());
+            commentsBinding = binding;
+        }
+
+        public void setData(CommentsResponse commentsResponse) {
+            commentsBinding.tvCommentAuthor.setText(commentsResponse.getAuthor());
+            commentsBinding.tvCommentBody.setText(commentsResponse.getComment());
+            commentsBinding.tvCommentTitle.setText(commentsResponse.getTitle());
+        }
+    }
+}
